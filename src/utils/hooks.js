44f05c92ba01fe3fda7ce7e3d85450cd57ce3686
Colin Gourlay <Gourlay.Colin@abc.net.au>
2019-04-11 10:51:25 +1000

    setSize->setHeight

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 export const useViewportHeight = () => {
   const [height, setHeight] = useState(window.innerHeight);
-  const onResize = () => setSize(window.innerHeight);
+  const onResize = () => setHeight(window.innerHeight);
 
   useEffect(() => {
     window.addEventListener('resize', onResize);
